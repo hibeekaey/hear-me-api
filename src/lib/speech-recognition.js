f25ef7cb5e00f879d78ab3cd279bbb54a4837889
Hibeekaey <hibeekaey@yahoo.com>
2018-10-23 08:50:52 +0100

    Update build

--- src/lib/speech-recognition.js
+++ src/lib/speech-recognition.js
@@ -21,11 +21,11 @@ export default function (req, res, next) {
   if (req.body.voice && req.body.language) { // check if voice is in request body
     // the audio file's encoding, sample rate in hertz, and BCP-47 language code
     // todo convert the received m4a file to wav
-    const audioWav = req.body.voice.toWav();
+    //const audioWav = req.body.voice.toWav();
 
     let request = {
       audio: {
-        content: audioWav,
+        content: req.body.voice,
       },
       config: {
         languageCode: req.body.language,
